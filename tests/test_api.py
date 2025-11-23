a89565459cc5894335b943ef133319aa4cd4a805
RdotF <tsarev02101977@gmail.com>
2025-11-23 16:54:38 +0300

    ch tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -3,28 +3,16 @@ import requests
 
 class TestAPI:
     
+    @pytest.mark.api
     def test_api_status(self):
         """Тест статуса API"""
         response = requests.get('https://httpbin.org/status/200')
         assert response.status_code == 200
     
+    @pytest.mark.api
     def test_api_json(self):
         """Тест JSON ответа"""
         response = requests.get('https://httpbin.org/json')
         assert response.status_code == 200
         data = response.json()
         assert 'slideshow' in data
-
-class TestUI:
-    
-    def test_ui_elements(self):
-        """Пример UI теста"""
-        # Здесь будет код Selenium тестов
-        assert True
-    
-    @pytest.mark.slow
-    def test_slow_operation(self):
-        """Медленный тест"""
-        import time
-        time.sleep(2)
-        assert True
